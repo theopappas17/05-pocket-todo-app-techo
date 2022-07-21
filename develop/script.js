1053bd0459ca81f12e8738548b999176ff93638a
theopappas17 <theopappas17@gmail.com>
2022-07-20 23:13:41 -0400

    Time pressure. Not finished. In progress. Subtmit anyway.

--- develop/script.js
+++ develop/script.js
@@ -3,19 +3,19 @@ let now = moment();
 //DOM Interactions
 let dateToday = document.getElementById('currentDay');
 let timeNow = document.getElementById('currentTime');
-let h800 = document.getElementById('800');
-let h900 = document.getElementById('900');
-let h1000 = document.getElementById('1000');
-let h1100 = document.getElementById('1100');
-let h1200 = document.getElementById('1200');
-let h1300 = document.getElementById('1300');
-let h1400 = document.getElementById('1400');
-let h1500 = document.getElementById('1500');
-let h1600 = document.getElementById('1600');
-let h1700 = document.getElementById('1700');
-let h1800 = document.getElementById('1800');
-let h1900 = document.getElementById('1900');
-let h2000 = document.getElementById('2000');
+let h800 = document.getElementById('h800');
+let h900 = document.getElementById('h900');
+let h1000 = document.getElementById('h1000');
+let h1100 = document.getElementById('h1100');
+let h1200 = document.getElementById('h1200');
+let h1300 = document.getElementById('h1300');
+let h1400 = document.getElementById('h1400');
+let h1500 = document.getElementById('h1500');
+let h1600 = document.getElementById('h1600');
+let h1700 = document.getElementById('h1700');
+let h1800 = document.getElementById('h1800');
+let h1900 = document.getElementById('h1900');
+let h2000 = document.getElementById('h2000');
 
 //DOM JS Insertions
 dateToday.innerHTML = now.format("dddd, MMMM Do YYYY");
@@ -26,7 +26,7 @@ function determineTimeBefore() {
   console.log(parseInt((now.format("HHMM"))));
   console.log(typeof(parseInt((now.format("HHMM")))));
   if (parseInt(now.format("HHMM")) < 800) {
-    h800.classList.add('past');
+    h800.style.backgroundColor = 'gray';
 } else if (parseInt(now.format("HHMM")) < 900) {
   h900.display.style.past;
 } else if (parseInt(now.format("HHMM")) < 1000) {
