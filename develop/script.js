cbef8c3e419fa164081e1d6051b9ee999da5b25a
theopappas17 <theopappas17@gmail.com>
2022-07-20 22:59:28 -0400

    Progress

--- develop/script.js
+++ develop/script.js
@@ -1,9 +1,55 @@
-//Access moment API
-const today = moment();
+let now = moment();
 
 //DOM Interactions
 let dateToday = document.getElementById('currentDay');
- 
-//DOM JS Insertions
-dateToday.innerHTML = today.format("dddd, MMMM Do YYYY");
+let timeNow = document.getElementById('currentTime');
+let h800 = document.getElementById('800');
+let h900 = document.getElementById('900');
+let h1000 = document.getElementById('1000');
+let h1100 = document.getElementById('1100');
+let h1200 = document.getElementById('1200');
+let h1300 = document.getElementById('1300');
+let h1400 = document.getElementById('1400');
+let h1500 = document.getElementById('1500');
+let h1600 = document.getElementById('1600');
+let h1700 = document.getElementById('1700');
+let h1800 = document.getElementById('1800');
+let h1900 = document.getElementById('1900');
+let h2000 = document.getElementById('2000');
 
+//DOM JS Insertions
+dateToday.innerHTML = now.format("dddd, MMMM Do YYYY");
+timeNow.innerHTML = now.format("HHMM");
+
+//Functions
+function determineTimeBefore() {
+  console.log(parseInt((now.format("HHMM"))));
+  console.log(typeof(parseInt((now.format("HHMM")))));
+  if (parseInt(now.format("HHMM")) < 800) {
+    h800.classList.add('past');
+} else if (parseInt(now.format("HHMM")) < 900) {
+  h900.display.style.past;
+} else if (parseInt(now.format("HHMM")) < 1000) {
+  h1000.display.style.past;
+} else if (parseInt(now.format("HHMM")) < 1200) {
+  h1200.display.style.past;
+} else if (parseInt(now.format("HHMM")) < 1300) { 
+    h1300.display.style.past;
+} else if (parseInt(now.format("HHMM")) < 1400) {
+    h1400.display.style.past;
+} else if (parseInt(now.format("HHMM")) < 1500) {
+    h1500.display.style.past;
+} else if (parseInt(now.format("HHMM")) < 1600) {
+    h1600.display.style.past;
+} else if (parseInt(now.format("HHMM")) < 1700) {
+    h1700.display.style.past;
+} else if (parseInt(now.format("HHMM")) < 1800) {
+  h1800.display.style.past;
+} else if (parseInt(now.format("HHMM")) < 1900) {
+  h1900.display.style.past;
+} else if (parseInt(now.format("HHMM")) < 2000) {
+  h2000.display.style.past;
+};
+}
+
+determineTimeBefore();
